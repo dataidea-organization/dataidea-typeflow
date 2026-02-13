93fa558a64589cd022bc127bbcc2206aa589c89e
jumashafara <jumashafara0@gmail.com>
2026-02-13 12:56:19 +0300

    feat: fixed ads

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/dataidea-typeflow">
         <PageViewTracker />
         <Routes>
           <Route path="/" element={<Index />} />
